a65846f4dedd15048c46aad1c618bcb7c6fdd8b9
MOVG08 <149404410+MOVG08@users.noreply.github.com>
2024-11-27 21:01:12 -0600

    algoritmo_congruencial_v2.py

--- algoritmo_congruencial.py
+++ algoritmo_congruencial.py
@@ -1,21 +1,14 @@
 import pandas as pd
 
-def generador_congruencial_lineal_mixto(seed, a, c, m, n):
+def metodo_congruencial_lineal(x0, a, c, m, n):
     """
     Generador de números pseudoaleatorios usando el algoritmo congruencial lineal mixto.
-
-    Parámetros:
-    - seed: semilla inicial (X0), debe ser mayor que 0.
-    - a: multiplicador, debe ser mayor que 0.
-    - c: constante aditiva, debe ser mayor que 0.
-    - m: módulo, debe ser mayor que X0, a, y c.
-    - n: cantidad de números pseudoaleatorios a generar.
 
     Retorna:
     - Una lista con los números generados.
     """
     resultados = []
-    x = seed  # Semilla inicial
+    x = x0
     
     for _ in range(n):
         # Calcular el siguiente número usando la fórmula del generador congruencial
@@ -25,14 +18,14 @@ def generador_congruencial_lineal_mixto(seed, a, c, m, n):
     return resultados
 
 # Parametros
-seed = 5
+x0 = 5   # Semilla
 a = 7  # Multiplicador
 c = 9  # Constante aditiva
-m = 11  # Módulo (usando 2^32, que es común en generadores de números aleatorios)
-n = 20  # Cantidad de números aleatorios a generar
+m = 11  # Módulo
+n = 20  # Cantidad de números a generar
 
 # Generar números pseudoaleatorios
-aleatorios = generador_congruencial_lineal_mixto(seed, a, c, m, n)
+aleatorios = metodo_congruencial_lineal(x0, a, c, m, n)
 
 # Crear una tabla con pandas para mostrar los resultados
 df = pd.DataFrame(aleatorios, columns=["Número Aleatorio"])
